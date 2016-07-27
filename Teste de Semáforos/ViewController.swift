7f40cfac86fe0081a3a26dcf6a8cf6d2cd0e9788
Alan Rabelo Martins <alan@macbook-pro-de-alan.local>
2016-07-26 21:08:12 -0300

    Initial Commit

---
+++ Teste de Semáforos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teste de Semáforos
+//
+//  Created by Alan Rabelo Martins on 7/26/16.
+//  Copyright © 2016 Alan Rabelo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
